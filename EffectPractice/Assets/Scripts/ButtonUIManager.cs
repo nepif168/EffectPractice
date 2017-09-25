db1c50b7befdbfca554983c87f5b941e824def92
Icchan <nepif168@gmail.com>
2017-09-25 12:14:12 +0900

    UI, EffectのLoadを追加

---
+++ EffectPractice/Assets/Scripts/ButtonUIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+
+public class ButtonUIManager : MonoBehaviour
+{
+    [SerializeField]
+    EffectLoader effectLoader;
+
+    [SerializeField]
+    Button effectListButton;
+
+    [SerializeField]
+    Transform content;
+
+    private void Awake()
+    {
+        effectLoader.Effects.ObserveAdd().Subscribe(effect =>
+        {
+            Instantiate(effectListButton, content).GetComponentInChildren<Text>().text = effect.Value.name;
+        });
+    }
+}
---
+++ EffectPractice/Assets/Scripts/EffectLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class EffectLoader : MonoBehaviour {
+
+    public ReactiveCollection<GameObject> Effects { get; private set; } = new ReactiveCollection<GameObject>();
+
+    private void Start()
+    {
+        foreach (var effect in Resources.LoadAll("Effects", typeof(GameObject)))
+            Effects.Add(effect as GameObject);
+    }
+
+}
