1989cc364693fa6fb29927226c20fc01185cc660
Icchan <miyake@at-crafts.com>
2017-09-29 18:34:36 +0900

    Effectの追加
    ヴァルキリーコネクトの
    https://gyazo.com/37e917a2779efec446ac2e84a555fc36
    これみたいなの作りました

---
+++ EffectPractice/Assets/EffectResources/MagicCircle/AutoRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class AutoRotation : MonoBehaviour {
+    [SerializeField] float rotationSpeed;
+
+    private void Start()
+    {
+        this.UpdateAsObservable().Subscribe(_ => transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed));
+    }
+}
